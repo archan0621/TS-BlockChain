586e4620c06bf7aeac9a72a042e81821258efff5
archan <joshua_0621@naver.com>
2021-05-20 16:11:14 +0900

    added class type

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human{
-    name: string;
-    age: number;
-    gender: string;
+class Human{
+    public name: string;
+    private age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender?:string){
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+    }
 }
 
-const person = {
-    name: "JJONG",
-    age: 19,
-    gender: "male"
-};
+const lynn = new Human("Lynn", 19, "female");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log (sayHi(person));
+console.log(sayHi(lynn));
 
 export{};
